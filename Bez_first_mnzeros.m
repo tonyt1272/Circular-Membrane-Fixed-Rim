beb7fa2f5b2fabe7377b870c60ebad2c12ec92e9
Anthony Thompson <34thompsonas@cua.edu>
2018-11-27 13:22:04 -0500

    First commit Circular Membrane Modes

---
+++ Bez_first_mnzeros.m
@@ -0,0 +1,48 @@
+function table = bez_first_mnzeros(m,num_zeros)
+%This functions creates a matrix that holds up to the nth zero of a bessel 
+%function of the first kind up to the m'th order.
+bzero=[];
+table=[];
+%m=5;
+num_zeros=num_zeros+1;       %number of zeros I want
+x=0:.0001:5*num_zeros;       %vector for the Bessel function operate on
+
+for j=1:m       %looping from 1 to m.  Special case at m=0 handled later
+    n=1;     %initializing counter for each zero, looking for n of them
+    i=0;     %initializing the index counter for tracing where in x
+    z = besselj(j,x);
+while (n-1) < num_zeros  %loop while the number of zeros found is less than
+                         %the num_zeros
+    i=i+1;          %advance index counter for x
+    if z(i)*z(i+1)<=0 %the zero check
+        value=(x(i)+x(i+1))/2; %taking the middle value for zero location
+        
+        if abs(value)<10^-2
+            value=0;    %set values near zero to zero
+        end
+        bzero=[bzero value]; %collect zero locations into row vector
+        n=n+1;  %advance counter for number of zeros found
+    end
+end
+table=[table ;bzero];  %create table for zeros at m,n 
+bzero=[];
+end
+n=1;
+i=0;
+z = besselj(0,x);     %Do the process one more time at m=0, the special
+                      %case.
+while (n-1) < num_zeros
+    i=i+1;
+    if z(i)*z(i+1)<=0
+        value=(x(i)+x(i+1))/2;
+        if value<10^-2
+            value=0;
+        end
+        bzero=[bzero value];
+        n=n+1;
+    end
+end
+bzero=bzero(1:end-1); %Drop the last value because we will add the NaN
+                      %to the front                      
+bzero=[NaN bzero];
+table =[bzero;table]; %Add the m=0 row to the top of the table
---
+++ Circular_Membrane_Modes.m
@@ -0,0 +1,70 @@
+clear
+clc
+close all
+close hidden
+
+%function y = find_n_zeros(x)
+    
+mn=Bez_first_mnzeros(10,10); %Calls the function that supplies a table that 
+                             %holds the J_m(j_mn) = 0.  You can make the 
+                             %table as big as you need.
+
+m=1;        %choose the mode to be plotted
+n=2;
+
+title_m=m;   %saving the m and values for the title       
+title_n=n;
+
+dr=.02;  %radial resolution
+a=1;  %radius of the circle
+r=0:dr:a;
+% theta=0:dtheta:2*pi-dtheta;
+
+dtheta=2*pi/80;  %angular resolution
+theta=0:dtheta:2*pi;    
+
+[R,Theta] = meshgrid(r,theta);
+
+
+
+mode_mn=besselj(m,mn(m+1,n+1)*R).*cos(m*Theta);
+%mode_02=besselj(1,R);
+X = R.*cos(Theta); 
+Y = R.*sin(Theta);
+[m,n]=size(R);
+
+surf(X,Y,mode_mn)
+title(['Circular Membrane, Fixed Rim, Mode(',num2str(title_m),...
+    ',',num2str(title_n),')'])
+
+% R = 1;
+% d_r=R/10;
+% d_theta=2*pi/50;
+% M = 100 ;
+% N = 100 ;
+% R1 = 0 ; % inner radius 
+% R2 = 1 ;  % outer radius
+% nR =0:d_r:10+d_r ;%linspace(R1,R2,M) ;
+% nT =0:d_theta:2*pi+d_theta ;%linspace(0,2*pi,N) ;
+% %nT = pi/180*(0:NT:theta) ;
+% [R, Theta] = meshgrid(nR,nT) ;
+% % Convert grid to cartesian coordintes
+% X = R.*cos(Theta); 
+% Y = R.*sin(Theta);
+% [m,n]=size(X);
+% % Plot grid
+% figure
+% set(gcf,'color','w') ;
+% axis equal
+% axis off
+% box on
+% hold on
+% % Plot internal grid lines
+% for i=1:m
+%     plot(X(i,:),Y(i,:),'k','linewidth',.05); 
+% end
+% for j=1:n
+%     plot(X(:,j),Y(:,j),'k','linewidth',.05); 
+% end
+% 
+% mode_01=besselj(0,R);
